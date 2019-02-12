75317f2655bed0dfab00995a8b155ecd8a822aec
Andrew Powell <andrew.powell_96@yahoo.co.uk>
2019-02-12 16:29:24 +0000

    sign up screen UI

--- app/src/main/java/uk/ac/tees/t7191599/agile_ica_0001/LogInActivity.java
+++ app/src/main/java/uk/ac/tees/t7191599/agile_ica_0001/LogInActivity.java
@@ -1,7 +1,9 @@
 package uk.ac.tees.t7191599.agile_ica_0001;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class LogInActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class LogInActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
+    }
+
+    public void signUP(View view)
+    {
+        Intent intent = new Intent(this, SignUPActivity.class);
+        startActivity(intent);
     }
 }
