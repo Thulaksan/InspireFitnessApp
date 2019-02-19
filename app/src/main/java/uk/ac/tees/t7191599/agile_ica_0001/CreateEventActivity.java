9e11b426758353dc9afbe268c47ba3d8cff98895
Andrew Powell <andrew.powell_96@yahoo.co.uk>
2019-02-19 16:53:54 +0000

    Event List UI added

---
+++ app/src/main/java/uk/ac/tees/t7191599/agile_ica_0001/CreateEventActivity.java
@@ -0,0 +1,16 @@
+package uk.ac.tees.t7191599.agile_ica_0001;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class CreateEventActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_event);
+    }
+}
--- app/src/main/java/uk/ac/tees/t7191599/agile_ica_0001/EventListActivity.java
+++ app/src/main/java/uk/ac/tees/t7191599/agile_ica_0001/EventListActivity.java
@@ -1,22 +1,28 @@
 package uk.ac.tees.t7191599.agile_ica_0001;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 public class EventListActivity extends AppCompatActivity {
-    ListView lv;
-    String[] s= new String[]{"apples", "apirs", "donkys", "newbirds", "cats", "Dogs"};
+    ListView listv;
+    String[] eventTypes= new String[]{"apples", "apirs", "donkys", "newbirds", "cats", "Dogs"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event_list);
-        lv = (ListView) findViewById(R.id.ListView);
-        ArrayAdapter ad = new ArrayAdapter(EventListActivity.this,android.R.layout.simple_expandable_list_item_1,s);
-        lv.setAdapter(ad);
-
+        listv = (ListView) findViewById(R.id.ListView);
+        ArrayAdapter adapter = new ArrayAdapter(EventListActivity.this,android.R.layout.simple_expandable_list_item_1,eventTypes);
+        listv.setAdapter(adapter);
+    }
+    public void CreateEvent(View view)
+    {
+        Intent intent = new Intent(this, CreateEventActivity.class);
+        startActivity(intent);
     }
 }
