bffc8bb32c535ae544cd2753c40f605530072bef
Andrew Powel <andrew.powell_96@yahoo.co.uk>
2019-02-28 17:14:21 +0000

    Added Event List To Database

--- app/src/main/java/uk/ac/tees/t7191599/agile_ica_0001/EventListActivity.java
+++ app/src/main/java/uk/ac/tees/t7191599/agile_ica_0001/EventListActivity.java
@@ -1,26 +1,46 @@
 package uk.ac.tees.t7191599.agile_ica_0001;
 
 import android.content.Intent;
-import android.os.Bundle;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class EventListActivity extends AppCompatActivity {
-    ListView listv;
-    String[] ev = new String[]{"apples", "apirs", "donkys", "newbirds", "cats", "Dogs"};
 
+    private eventAdapter adapter;
+    private ListView listViewEvents;
+    private ArrayList<Event> events = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event_list);
-        listv = (ListView) findViewById(R.id.eventListView);
-        ArrayAdapter adapter = new ArrayAdapter(EventListActivity.this, android.R.layout.simple_expandable_list_item_1, ev);
-        listv.setAdapter(adapter);
+        populateListView();
+
+
+    }
+    public void populateListView()
+    {
+        listViewEvents = (ListView) findViewById(R.id.eventListView);
+        events.add(new Event("Step Tracker","22/06/2020"));
+        events.add(new Event("Workout","01/12/2019"));
+        events.add(new Event("Meal Plan","14/08/2019"));
+        events.add(new Event("Workout","11/12/2019"));
+        adapter= new eventAdapter(this,events);
+        listViewEvents.setAdapter(adapter);
+
     }
 
-    public void CreateEvent(View view) {
+
+
+
+    public void CreateEvent(View view)
+    {
         Intent intent = new Intent(this, CreateEventActivity.class);
         startActivity(intent);
     }
--- app/src/main/java/uk/ac/tees/t7191599/agile_ica_0001/LogInActivity.java
+++ app/src/main/java/uk/ac/tees/t7191599/agile_ica_0001/LogInActivity.java
@@ -29,7 +29,7 @@ public class LogInActivity extends AppCompatActivity {
         Intent intent = new Intent(this, SignUPActivity.class);
         startActivity(intent);
     }
-    public void LoggedIn() {
+    public void LoggedIn(View view) {
         System.out.println("Thomas");
         Intent intent = new Intent(this, LogInActivity.class);
         startActivity(intent);
@@ -37,7 +37,7 @@ public class LogInActivity extends AppCompatActivity {
 
 
 
-    public void signIn(View view) {
+    public void signIn(final View view) {
 
         FirebaseFirestore db = FirebaseFirestore.getInstance();
         FirebaseFirestoreSettings settings = new FirebaseFirestoreSettings.Builder()
@@ -66,7 +66,7 @@ public class LogInActivity extends AppCompatActivity {
                         if (TempUser.Password.equals(Password)) {
 
                             Toast.makeText(LogInActivity.this, TempUser.Email+" "+TempUser.First_Name+" "+TempUser.Second_Name, Toast.LENGTH_SHORT).show();
-                            LoggedIn();
+                            LoggedIn(view);
                         }
 
                     }
