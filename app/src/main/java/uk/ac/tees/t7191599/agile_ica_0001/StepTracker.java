734e5fa6136ac93b054a711920937acc54dd2f58
THOMAS BLACK <T7066356@live.tees.ac.uk>
2019-04-24 08:52:25 +0100

    Add New Step Tracker Activity

---
+++ app/src/main/java/uk/ac/tees/t7191599/agile_ica_0001/StepTracker.java
@@ -0,0 +1,13 @@
+package uk.ac.tees.t7191599.agile_ica_0001;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StepTracker extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_step_tracker);
+    }
+}
