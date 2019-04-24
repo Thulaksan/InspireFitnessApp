efd85571dc7daa6bd91a26824bfd827028d6f2bf
THOMAS BLACK <T7066356@live.tees.ac.uk>
2019-04-24 08:49:49 +0100

    Change Main Start Activity to Login

--- app/src/main/java/uk/ac/tees/t7191599/agile_ica_0001/MainActivity.java
+++ app/src/main/java/uk/ac/tees/t7191599/agile_ica_0001/MainActivity.java
@@ -12,7 +12,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Intent intent = new Intent(this, LogInActivity.class);
-        startActivity(intent);
+
     }
 
     public void gottologin(View view) {
